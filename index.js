50d8ca535e147af038ab4b3cf3ba306678e906c4
Anup Kumar Panwar <mylnext@gmail.com>
2016-08-27 12:46:13 +0530

    Insatlled Modules

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var path = require('path');
 app.get('/', function(req, res){
   var express=require('express');
   app.use(express.static(path.join(__dirname)));
-  res.sendFile(path.join(__dirname, '../SpidyChatApp', 'index.html'));
+  res.sendFile(path.join(__dirname, '../NodeJS-SocketIO-Realtime-Chat-App', 'index.html'));
 });
  
 // Register events on socket connection
